7dfe6a8c695ede1dde908555da3eb9794b5ddaad
Kaofang <benyapa_kh@cmu.ac.th>
2021-01-19 17:14:20 +0700

    upd_paint

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -21,3 +21,23 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool p[][M]){
+    cout<<"------------------------------------------------------------------------\n";
+    for(int i=0;i<N;i++){ 
+    cout<<"|";
+    for(int j=0;j<70;j++){
+    if(p[i][j]==1) cout<<"*";
+    else cout << " ";
+    }
+    cout<<"|\n";
+    }
+    cout<<"------------------------------------------------------------------------\n";
+}
+void updateImage(bool p[][M],int s,int x,int y){
+    for(int i=0;i<N;i++){ 
+        for(int j=0;j<70;j++){
+            if(sqrt(pow((i-x),2)+pow((j-y),2)) <= s-1) p[i][j]=1;
+            }
+        }
+    }
